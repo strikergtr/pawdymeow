a821f3e9468d21f4d8b38c922e5d0f3e8bd63f4d
DESKTOP-6UPT102\StrikerXE10 <cotton989@gmail.com>
2025-11-11 10:00:54 +0700

    no message

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-website/',
+  base: '/pawdymeow/',
   server: {
     host: 'localhost',
     port: 5173,
